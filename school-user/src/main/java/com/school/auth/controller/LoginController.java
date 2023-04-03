f09cd5aae18a812f81a0006ff5e95b5ce3a69bf0
wang6966 <2066229586@qq.com>
2023-04-03 20:22:19 +0800

    ver1.0.0

--- school-user/src/main/java/com/school/auth/controller/LoginController.java
+++ school-user/src/main/java/com/school/auth/controller/LoginController.java
@@ -1,10 +1,20 @@
 package com.school.auth.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @Description TODO
  * @ClassName LoginController
  * @Author 龙涛05664416459
  * @Date 2023/4/3 17:04
  **/
+@RestController
+@RequestMapping("/test")
 public class LoginController {
+    @GetMapping("/test1")
+    public String  test1(){
+        return"OK1，successful！！！";
+    }
 }
