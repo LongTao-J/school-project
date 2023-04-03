898e6e1c0fdeaf7f5cce2787c2bd36a9ce62a2bf
longtaonb <1649039895@qq.com>
2023-04-03 20:27:32 +0800

    初始工程

--- school-user/src/main/java/com/school/auth/controller/LoginController.java
+++ school-user/src/main/java/com/school/auth/controller/LoginController.java
@@ -1,10 +1,15 @@
 package com.school.auth.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @Description TODO
  * @ClassName LoginController
  * @Author 龙涛05664416459
  * @Date 2023/4/3 17:04
  **/
+@RestController
+@RequestMapping
 public class LoginController {
 }
--- school-user/src/test/java/com/school/SchoolUserApplicationTests.java
+++ school-user/src/test/java/com/school/SchoolUserApplicationTests.java
@@ -1,13 +1,18 @@
 package com.school;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SchoolUserApplicationTests {
+    @Autowired
+    SUserService sUserService;
 
     @Test
     void contextLoads() {
+        SUser sUser = sUserService.queryById("0x00009127");
+        System.out.println(sUser);
     }
 
 }
